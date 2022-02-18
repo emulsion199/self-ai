5d570298131eef790905b1bb470bc0e95c816a00
CHLee0801 <ckdgh0801@korea.ac.kr>
2022-02-19 01:58:31 +0900

    button update

--- src/components/MainComponent/Question/Question.js
+++ src/components/MainComponent/Question/Question.js
@@ -1,6 +1,6 @@
 import MoveButton from "./MoveButton"
 import QuestionBalloon from "./QuestionBalloon"
-
+import {useState,useEffect,useRef} from 'react'
 const question_list=
 [
     'How do you relieve stress?',
@@ -23,7 +23,20 @@ const question_list=
 const Question=(args)=>
 {   
     const type= args.type
-    const num = 1
+    
+    const [page_num, setPage_num]= useState(1)
+    const [left,setLeft]= useState(false)
+    const [right,setRight] = useState(true)
+    const onLeftMove = (e) => {
+        if (page_num > 1) {
+            setPage_num(page_num-1)
+        }
+    };
+    const onRightMove = (e) => {
+        if (page_num < 3) {
+            setPage_num(page_num+1)
+        }
+    };
     return(
         <div>
             <div style={{
@@ -50,15 +63,26 @@ const Question=(args)=>
                 fontWeight:'500',
                 fontSize:'125%'
                 }}>
-                {num} / 3
+                {page_num} / 3
                 </div>
             </div>
-        <QuestionBalloon question={question_list[0 + 5*(num-1)]}></QuestionBalloon>
-        <QuestionBalloon question={question_list[1 + 5*(num-1)]}></QuestionBalloon>
-        <QuestionBalloon question={question_list[2 + 5*(num-1)]}></QuestionBalloon>
-        <QuestionBalloon question={question_list[3 + 5*(num-1)]}></QuestionBalloon>
-        <QuestionBalloon question={question_list[4 + 5*(num-1)]}></QuestionBalloon>
-        <MoveButton></MoveButton>
+        <QuestionBalloon question={question_list[0 + 5*(page_num-1)]}></QuestionBalloon>
+        <QuestionBalloon question={question_list[1 + 5*(page_num-1)]}></QuestionBalloon>
+        <QuestionBalloon question={question_list[2 + 5*(page_num-1)]}></QuestionBalloon>
+        <QuestionBalloon question={question_list[3 + 5*(page_num-1)]}></QuestionBalloon>
+        <QuestionBalloon question={question_list[4 + 5*(page_num-1)]}></QuestionBalloon>
+        <div style={{
+                display:'grid',
+                gridTemplateColumns:'1fr 1fr',
+                paddingTop:'15%',
+                marginBottom:'-5%',
+                marginLeft:'9%',
+                width:'82%'
+            }}>
+                <MoveButton direction={'<'}></MoveButton>
+                <MoveButton direction={'>'}></MoveButton>
+            </div>
+        
         </div>
     )
 }
