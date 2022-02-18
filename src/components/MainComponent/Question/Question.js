97d7f7946c175d5cd7f8cb086e405d1ec08f1242
CHLee0801 <ckdgh0801@korea.ac.kr>
2022-02-19 02:21:09 +0900

    question table completed

--- src/components/MainComponent/Question/Question.js
+++ src/components/MainComponent/Question/Question.js
@@ -1,4 +1,3 @@
-import MoveButton from "./MoveButton"
 import QuestionBalloon from "./QuestionBalloon"
 import {useState,useEffect,useRef} from 'react'
 const question_list=
@@ -20,21 +19,49 @@ const question_list=
     'What do you do for fun?',
     'Who is your favorite artist?'
 ]
+const a = '<'
+const b = '>'
+const css={
+    unClicked:
+    {   
+        color:'white',
+        background:'rgba(18,27,40,0.4)',
+        textAlign:'center',
+        justifyContent:'center',
+        borderRadius:'5px',
+        fontWeight:'400',
+        fontSize:'200%',
+        marginLeft:'3%',
+        marginRight:'3%',
+        marginTop:'-30%'
+    },
+    Clicked:
+    {
+        color:'white',
+        background:'rgba(18,27,40,0.8)',
+        textAlign:'center',
+        justifyContent:'center',
+        borderRadius:'5px',
+        fontWeight:'400',
+        fontSize:'200%',
+        marginLeft:'3%',
+        marginRight:'3%',
+        marginTop:'-30%'
+    }
+}
 const Question=(args)=>
 {   
     const type= args.type
     
     const [page_num, setPage_num]= useState(1)
-    const [left,setLeft]= useState(false)
-    const [right,setRight] = useState(true)
     const onLeftMove = (e) => {
         if (page_num > 1) {
-            setPage_num(page_num-1)
+            setPage_num(page_num-1);
         }
     };
     const onRightMove = (e) => {
         if (page_num < 3) {
-            setPage_num(page_num+1)
+            setPage_num(page_num+1);
         }
     };
     return(
@@ -79,8 +106,8 @@ const Question=(args)=>
                 marginLeft:'9%',
                 width:'82%'
             }}>
-                <MoveButton direction={'<'}></MoveButton>
-                <MoveButton direction={'>'}></MoveButton>
+                <div onClick={onLeftMove} style={page_num==1? css.unClicked:css.Clicked}> {a} </div>
+                <div onClick={onRightMove} style={page_num==3? css.unClicked:css.Clicked}> {b} </div>
             </div>
         
         </div>
